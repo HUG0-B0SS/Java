761abbbf04012b3e2265d11a929825f9a50ade61
HUG0-B0SS <116866455+HUG0-B0SS@users.noreply.github.com>
2023-10-05 23:08:26 +0300

    Create h1.java

---
+++ h1.java
@@ -0,0 +1,13 @@
+public class h1 {
+
+    public static void main(String[] args) {
+        String nimi = "Hugo-Hendrik Vainlu";
+        String kursus = "IT-22";
+        int vanus = 17;
+        double pikkus = 1.71;
+        String huudnimi = "Boss";
+
+        System.out.println(nimi + ", " + kursus);
+        System.out.println("vanus: " + vanus + " aastat, pikkus: " + pikkus + "m, hüüdnimi: " + huudnimi);
+    }
+}
